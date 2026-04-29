30a9be8b3a8754b458ee0b706906f36d2e5209c0
Kimyiunseo <palaengi87@gmail.com>
2026-04-29 15:43:01 +0900

    시작

---
+++ Ui A KYS/Assets/LeaderBoard.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+
+public class LeaderBoard : MonoBehaviour
+{
+    public TextMeshProUGUI stage1;
+    public TextMeshProUGUI stage2;
+    public TextMeshProUGUI stage3;
+    public TextMeshProUGUI stage4;
+    public TextMeshProUGUI stage5;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        stage1.text = "Stage 1 : " + HighScore.Load(1).ToString();
+        stage1.text = "Stage 2 : " + HighScore.Load(2).ToString();
+        stage1.text = "Stage 3 : " + HighScore.Load(3).ToString();
+        stage1.text = "Stage 4 : " + HighScore.Load(4).ToString();
+        stage1.text = "Stage 5 : " + HighScore.Load(5).ToString();
+    }
+}
